4c4e50a353b2a3c136ee7fd61641adae9a5ae520
user <user@FACE-8>
2019-09-20 11:12:20 +0530

    complete 1 project

---
+++ height/src/height/Height.java
@@ -0,0 +1,38 @@
+package height;
+
+import java.util.Scanner;
+
+public class Height {
+	public static void main(String[] args)
+	{
+		int n, temp;
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter the no of students ");
+		n = s.nextInt();
+		int a[] = new int[n];
+		System.out.println("Enter the height of students:");
+		for (int i = 0; i < n; i++) 
+		{
+			a[i] = s.nextInt();
+		}
+		for (int i = 0; i < n; i++) 
+		{
+			for (int j = i + 1; j < n; j++) 
+			{
+				if (a[i] > a[j]) 
+				{
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		System.out.println("height is in the order as");
+		for (int i = 0; i < n ; i++) 
+		{
+			System.out.println(a[i]);
+		}
+	}   
+}
+
+
